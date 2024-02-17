62f25053dacfeff23505ac77840ee4138497fad3
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2024-02-17 22:40:55 +0100

    update devcont

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,12 +1,3 @@
-#!/bin/sh
-
-# Install the version of Bundler.
-if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null; then
-    cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
-fi
-
-# If there's a Gemfile, then run `bundle install`
-# It's assumed that the Gemfile will install Jekyll too
-if [ -f Gemfile ]; then
-    bundle install
-fi
+#!/bin/bash
+gem install bundler -v 2.4.22
+bundle install
